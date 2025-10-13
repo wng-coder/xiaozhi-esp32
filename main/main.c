29c86e5abee8288d1e2e462efe2266140e47e17a
ROS-education <admin@ros.education>
2025-10-13 08:16:06 +0700

    Stable Lua REPL, memory safety improvements, TinyUSB removed, codebase cleanup

--- main/main.c
+++ main/main.c
@@ -13,17 +13,36 @@
 #include "linenoise/linenoise.h"
 #include "esp_vfs_dev.h"
 
+#include <stdlib.h>
+
 
 #define TAG "main"
 #define BUF_SIZE 4096
 
 
 
-
+static void *esp_idf_lua_alloc(void *ud, void *ptr, size_t osize, size_t nsize) {
+    (void)ud; (void)osize;
+    if (nsize == 0) {
+        free(ptr);
+        return NULL;
+    } else {
+        return realloc(ptr, nsize);
+    }
+}
 
 static int lua_repl_cmd(int argc, char **argv)
 {
-    lua_State *L = luaL_newstate();
+    size_t heap_before = esp_get_free_heap_size();
+    if (heap_before < 65536) {
+        printf("Warning: low heap before Lua init: %lu\n", (unsigned long)heap_before);
+    }
+
+    lua_State *L = lua_newstate(esp_idf_lua_alloc, NULL);
+    if (!L) {
+        printf("Failed to create Lua state!\n");
+        return 1;
+    }
     luaL_openlibs(L);
     printf("Lua REPL started. Type Lua code. End with a single dot '.' on a line.\n");
     char data[BUF_SIZE];
@@ -71,6 +90,7 @@ void app_main(void)
     esp_console_repl_t *repl = NULL;
     esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
     repl_config.prompt = "repl >";
+    repl_config.task_stack_size = 8192; // Increase stack size for Lua
     const esp_console_cmd_t lua_cmd = {
         .command = "lua",
         .help = "Start Lua REPL (end input with '.')",
